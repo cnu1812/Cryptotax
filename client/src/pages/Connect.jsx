c42a5a6e95c7d1280f357a9c7763ec61d8108269
Miguel Bertol <miguelbertolcodes@gmail.com>
2023-05-20 14:19:59 -0300

    feat: connect wallet using walletconnect

--- client/src/pages/Connect.jsx
+++ client/src/pages/Connect.jsx
@@ -1,79 +1,18 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import { formatEther } from "ethers"
 import Navbar from '../components/Navbar'
 import Breadcrumb from '../components/Breadcrumb'
+import ConnectWalletButton from '../components/ConnectWalletButton'
+
+/*TODO:
+1. Manage states between the wallet button and select input
+2. Send the wallet addres and location to the backend
+3. Send data between react-routes, that is, send the location to the dashboard.
+4. Add prettier to the project
+5. Costumize wallet modal
+*/
 
 function Connect(){
-    const [errorMessage, setErrorMessage] = useState(null);
-    const [account, setAccount] = useState(null);
-    const [balance, setBalance] = useState(null);
-    const [chain, setChain] = useState(null);
-    const [network, setNetwork] = useState(null);
-    const [currency, setCurrency] = useState(null);
-  
-    useEffect(() => {
-      if (window.ethereum) {
-        window.ethereum.on("accountsChanged", accountsChanged);
-        window.ethereum.on("chainChanged", chainChanged);
-      }
-    }, []);
-  
-    const connectHandler = async () => {
-      if (window.ethereum) {
-        try {
-          const res = await window.ethereum.request({
-            method: "eth_requestAccounts",
-          });
-          await accountsChanged(res[0]);
-        } catch (err) {
-          console.error(err);
-          setErrorMessage("There was a problem connecting to MetaMask");
-        }
-      } else {
-        setErrorMessage("Install MetaMask");
-      }
-    };
-  
-    const accountsChanged = async (newAccount) => {
-      setAccount(newAccount);
-      try {
-        const balance = await window.ethereum.request({
-          method: "eth_getBalance",
-          params: [newAccount.toString(), "latest"],
-        });
-        setBalance(formatEther(balance));
-        const chainId = await window.ethereum.request({
-            method: 'eth_chainId'
-        });
-        setChain(chainId)
-        switch (chainId) {
-            case '0xaa36a7':
-                setNetwork('Sepolia test network');
-                setCurrency('SepoliaETH');
-                break;
-            case '0x5':
-                setNetwork('Goerli test network');
-                setCurrency('GoerliETH');
-                break;
-            default:
-                setNetwork(null);
-                setCurrency(null);
-        }
-      } catch (err) {
-        console.error(err);
-        setErrorMessage("There was a problem connecting to MetaMask");
-      }
-    };
-  
-    const chainChanged = () => {
-      setErrorMessage(null);
-      setAccount(null);
-      setBalance(null);
-      setChain(null);
-      setNetwork(null);
-      setCurrency(null);
-    };
-
 
     return(
         <>
@@ -92,25 +31,13 @@ function Connect(){
                                     <option value="brazil">Brazil</option>
                                 </select>
                             </div>
-                            <button
-                                    type='button'
-                                    onClick={connectHandler} 
-                                    className="block bg-teal-500 text-center text-neutral-50 text-sm font-medium font-title uppercase p-3 rounded-md">
-                                        Connect your wallet
-                            </button>
+                            <ConnectWalletButton />
                         </form>
                     </section>
                     <section className="bg-neutral-200 grid place-items-center">
                         <div className="text-center text-neutral-400 font-semibold">img here</div>
                     </section>
                 </section>
-                <p>Account: {account}</p>
-                <p>Balance: {balance} {currency}</p>
-                <p>Chain ID: {chain}</p>
-                <p>Network: {network}</p>
-                {errorMessage ? (
-                    <p>Error: {errorMessage}</p>
-                ) : null}
             </main>
         </>
         
